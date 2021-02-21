2384dbbc7c6c0742627ba9e8d581ae76b00ede0e
bhupesh7shakya <54110001+bhupesh7shakya@users.noreply.github.com>
2021-02-20 19:07:24 -0800

    Update productpageReview.php

--- bestdeal/productpageReview.php
+++ bestdeal/productpageReview.php
@@ -74,6 +74,7 @@
                         $udResult = mysqli_query($con, $updateSql);
                         if ($udResult) {
                             echo 'updated ';
+                            
                         } else {
                             echo 'not updated';
                         }
@@ -128,12 +129,12 @@
                             <div class="float-right mb-5" style="transform: translate(-160%,101%);">
                             <form action=' . $_SERVER["REQUEST_URI"] . ' class="position-absolute mx-5" method="POST">
                             <input type="hidden" value="' . $rows['comment_id'] . '" name="delete" class=" p-2">
-                            <button type="submit" class="btn btn-outline-danger"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
+                            <button type="submit" id="delete"  class="btn btn-outline-danger"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
                             <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/>
                           </svg></button>
                           </form>
                           
-                          <button onclick="edit(' . $rows['comment_id'] . ')" class="btn btn-outline-success"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
+                          <button id="update" onclick="edit(' . $rows['comment_id'] . ')" class="btn btn-outline-success"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
                           <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456l-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
                           <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
                         </svg></button>
@@ -182,12 +183,16 @@
     </div>
     </div>
     <script>
+        //function that takes the para meter that is the id of the  comment
         function edit(a) {
+            document.getElementById('delete').style.opacity="0";
+            document.getElementById('update').style.opacity="0";
+
             console.log(a);
             comment = document.getElementById(a).innerHTML;
-           document.getElementById('a').innerHTML='<form method="POST"><input type="hidden" value="' + a +
+            document.getElementById(a).innerHTML='<form method="POST"><input type="hidden" value="' + a +
                 '" name="id"><textarea name="updatedComment" rows="3" cols=130%>' + comment +
-                '</textarea><button type="submit">Update</button></form>';
+                '</textarea><button class="float-right btn btn-outline" type="submit">Update</button></form>';
         }
     </script>
     <!-- Optional JavaScript -->
